36bdf87620e0e68d0223e833b5e61fe2d082a4fc
AceDJH <460745177@qq.com>
2019-10-08 23:24:02 +0800

    Update config.py

--- www/config.py
+++ www/config.py
@@ -34,7 +34,7 @@ def merge(defaults, override):
     r = {}
     for k, v in defaults.items():
         if k in override:
-            if instance(v, dict):
+            if isinstance(v, dict):
                 r[k] = merge(v, override[k])
             else:
                 r[k] = override[k]
