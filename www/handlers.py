e2ab3eefa8ea6d93c3d256619512c687e68fc024
AceDJH <460745177@qq.com>
2019-10-08 21:14:16 +0800

    Update handlers.py

--- www/handlers.py
+++ www/handlers.py
@@ -20,8 +20,24 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'lalala'
+    blogs = [
+        Blog(id='1', name='Blog1', summary=summary,
+             created_at=time.time() - 120),
+        Blog(id='2', name='Blog2', summary=summary,
+             created_at=time.time() - 3600),
+        Blog(id='3', name='Blog3', summary=summary,
+             created_at=time.time() - 4000)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
+
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
