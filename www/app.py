c2a00ca48d7a60bf72b47cb77f822d996712a6be
AceDJH <460745177@qq.com>
2019-10-08 21:14:12 +0800

    Update app.py

--- www/app.py
+++ www/app.py
@@ -85,7 +85,7 @@ async def response_factory(app, handler):
             template = r.get('__template__')
             if template is None:
                 resp = web.Response(body=json.dumps(
-                    r, ensure_ascii=False, dafault=lambda o: o.__dict__).encode('utf-8'))
+                    r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
                 resp.content_type = 'application/json;charset = utf-8'
                 return resp
             else:
@@ -110,7 +110,7 @@ def datetime_filter(t):
     delta = int(time.time() - t)
     if delta < 60:
         return u'1分钟前'
-    if delte < 3600:
+    if delta < 3600:
         return u'%s分钟前' % (delta // 60)
     if delta < 86400:
         return u'%s小时前' % (delta // 3600)
