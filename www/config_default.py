67b9043292c0debca8419d7c66e3293f49da4fc7
AceDJH <460745177@qq.com>
2019-10-17 15:19:18 +0800

    Update config_default.py

--- www/config_default.py
+++ www/config_default.py
@@ -14,7 +14,8 @@ configs = {
         'port': 3306,
         'user': 'www-data',
         'password': 'www-data',
-        'db': 'awesome'
+        'db': 'awesome',
+        'charset': 'utf8'
     },
     'session': {
         'secret': 'Awesome'
